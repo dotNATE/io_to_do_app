e231b3e1e0903b9b33fdd6064550652d0d3d5f5a
Nathaniel Payne <somersetnathaniel@gmail.com>
2021-05-03 16:38:42 +0100

    refactored displayTodoArray into async func

--- todo.js
+++ todo.js
@@ -1,17 +1,20 @@
-let form = document.querySelector('form')
-let todoListDisplay = document.querySelector('#todoListItems')
-let themeToggleButton = document.querySelector('#toggleThemeButton')
+function getTodosFromLocalStorage() {
+    return JSON.parse(localStorage.getItem('todos'))
+}
 
-let todoItems = { todos: JSON.parse(localStorage.getItem('todos')) }
+async function displayTodoArray(todoArray) {
+    const templateData = await fetch('todo_list.hbs')
+    const templateText = await templateData.text()
+    const template = await Handlebars.compile(templateText)
+    todoListDisplay.innerHTML = await template({todos: todoArray})
+}
 
-function displayTodoArray(todoArray) {
-    fetch('todo_list.hbs')
-        .then((data) => data.text())
-        .then((data) => {
-            const template = Handlebars.compile(data)
-            const html = template(todoArray)
-            todoListDisplay.innerHTML = html
-        })
+function addToDoEventListeners() {
+    let todoCheckboxes = document.querySelectorAll('.checkbox')
+    let todoDeleteCrosses = document.querySelectorAll('.listItemCross')
+
+    console.log(todoCheckboxes)
+    console.log(todoDeleteCrosses)
 }
 
 function toggleTheme() {
@@ -19,6 +22,19 @@ function toggleTheme() {
     if (themeStyleSheet.href.includes('dark')) {
         themeStyleSheet.href = 'http://localhost:1234/todo-app-main/styles/light_theme.css'
     } else themeStyleSheet.href = 'http://localhost:1234/todo-app-main/styles/dark_theme.css'
+}
+
+let form = document.querySelector('form')
+let todoListDisplay = document.querySelector('#todoListItems')
+let themeToggleButton = document.querySelector('#toggleThemeButton')
+
+let todoItems = getTodosFromLocalStorage()
+
+if (!todoItems) {
+    todoItems = []
+} else {
+    displayTodoArray(todoItems)
+        .then(() => addToDoEventListeners())
 }
 
 themeToggleButton.addEventListener('click', toggleTheme)
@@ -29,21 +45,36 @@ form.addEventListener('submit', (e) => {
     let todoInput = document.querySelector('#todoInput')
 
     if (todoInput.value !== '' && todoInput.value !== null) {
+
         let todo = {
             id: new Date().getTime(),
             name: todoInput.value,
             isCompleted: false
         }
+
         todoItems.push(todo)
         localStorage.setItem('todos', JSON.stringify(todoItems))
         form.reset()
     }
-
+    displayTodoArray(todoItems)
+        .then(() => addToDoEventListeners())
     todoInput.focus()
 })
 
-if (!todoItems.todos) {
-    todoItems = []
-} else {
-    displayTodoArray(todoItems)
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
